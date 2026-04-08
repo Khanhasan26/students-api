9f0f2e1eea9dc05d6137f3473670d8ed57243961
Khanhasan26 <hasanmunafkhan26@gmail.com>
2026-04-08 12:12:27 +0530

    initial commit

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Khanhasan26/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("welcome to students-api")
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	fmt.Printf("Server Started %s\n", cfg.HTTPServer.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Failed to start server")
+	}
+
 }
